291ef1ed6044311ad4b41d33b92614f5f6dce730
sainisarthak4321 <sainisarthak4321@gmail.com>
2018-08-04 10:14:01 -0500

    process in parallel

--- scrape_google_scorecard.R
+++ scrape_google_scorecard.R
@@ -1,15 +1,20 @@
+## get all the required packages
 library(rvest)
 library(Rcrawler)
 library(dplyr)
 library(magrittr)
+library(foreach)
+library(doParallel)
 
+## Settings
 data_directory <- "D:/cricket/web_data/google_scorecard_files"
 all_files <- list.files(data_directory, c(".html"))
 
-batsman_data_all <- data.frame()
-bowler_data_all <- data.frame()
 
-for(i in 1 : length(all_files))
+scrapping_clusters = makeCluster(5)
+registerDoParallel(scrapping_clusters)
+
+batsman_data_all = foreach(i = 1 : length(all_files), .packages=c("dplyr", "rvest", "magrittr")) %dopar%
 {
   
   data_cricinfo <- read_html(file.path(data_directory, all_files[[i]]))
@@ -31,7 +36,13 @@ for(i in 1 : length(all_files))
     dplyr::filter(runs != "")%>%
     dplyr::mutate(stadium = stadium_name[1,])
   
-  
+  batsman_data_all <- rbind(batsman_data_all, batsman_data)
+  bowler_data_all <- rbind(bowler_data_all, bowler_data)
+}
+
+bowler_data_all = foreach(i = 1 : length(all_files), .packages=c("dplyr", "rvest", "magrittr")) %dopar%
+{
+  data_cricinfo <- read_html(file.path(data_directory, all_files[[i]]))
   bowler_data_raw <- html_nodes(data_cricinfo, ".imspo_tps__rbpwod")
   bowler_data_raw <- data.frame(html_text(bowler_data_raw))
   
@@ -46,6 +57,6 @@ for(i in 1 : length(all_files))
   bowler_data <- bowler_data %>%
     dplyr::filter(overs != "")
   
-  batsman_data_all <- rbind(batsman_data_all, batsman_data)
-  bowler_data_all <- rbind(bowler_data_all, bowler_data)
-}
+  bowler_data
+}  
+stopCluster(scrapping_clusters)
